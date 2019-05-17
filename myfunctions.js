34bbbfdfccf778a3a68bc8904090d8d2c71e4ea0
Tyler Fung <49625982+fungt1@users.noreply.github.com>
2019-05-16 22:24:08 -0400

    Update with more scrolling function

--- myfunctions.js
+++ myfunctions.js
@@ -1,15 +1,25 @@
-$(document).ready(function() {
+  $(window).scroll(function() {
+            if ($(document).scrollTop() > 50) {
+                $('.nav').addClass('affix');
+                console.log("OK");
+            } else {
+                $('.nav').removeClass('affix');
+            }
+        });
 
-    $(window).scroll(function() {
+$('.navTrigger').click(function () {
+    $(this).toggleClass('active');
+    console.log("Clicked menu");
+    $("#mainListDiv").toggleClass("show_list");
+    $("#mainListDiv").fadeIn();
+});
 
-        var height = $('.first-container').height();
-        var scrollTop = $(window).scrollTop();
-
-        if (scrollTop >= height - 40) {
-            $('.nav-container').addClass('solid-nav');
-        } else {
-            $('.nav-container').removeClass('solid-nav');
-        }
-
-    });
+$(function() {
+$(document).on('click','a.page-scroll',function(event){
+  var $anchor=$(this);
+  $('html,body').stop().animate({
+    scrollTop: ($($anchor.attr('href')).offset().top-45)
+  },1500,'easeInOutExpo');
+  event.preventDefault();
+});
 });
